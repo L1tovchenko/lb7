e899de2ab28007d00f245026fd511e79ed9e6f49
L1tovchenko <119450776+L1tovchenko@users.noreply.github.com>
2022-12-02 22:06:29 +0200

    Create lb11.cpp
    Hello

---
+++ lb11.cpp
@@ -0,0 +1,32 @@
+﻿// lb11.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+#include <iostream>
+#include <cstring>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Введіть ваше слово: ";
+    char Word[10];
+    cin >> Word;
+    int a = 0;
+    for (int n = 0; n < strlen(Word); n++)
+    {
+        for (int i = 47;i < 58; i++)
+        {
+            if (Word[n] == (char)i)
+            {
+            a++;
+            }
+        }
+    };
+    if (a == 1)
+        cout << "Ваше слово " << Word << " має лише одну цифру." << endl;
+    else 
+        cout << "Ваше слово не має жодної цифри." << endl;
+}
